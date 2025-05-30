554b8a846cfad78b4f5c78c132ff8955e274d047
Anh140402 <anhnhmse181874@fpt.edu.vn>
2025-05-30 10:51:23 +0700

    setup enviroment

---
+++ back-end/src/main/java/SWD392/Bloodline_DNA_Testing_System/ServletInitializer.java
@@ -0,0 +1,13 @@
+package SWD392.Bloodline_DNA_Testing_System;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(BackendApplication.class);
+	}
+
+}
---
+++ back-end/src/test/java/SWD392/Bloodline_DNA_Testing_System/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package SWD392.Bloodline_DNA_Testing_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
