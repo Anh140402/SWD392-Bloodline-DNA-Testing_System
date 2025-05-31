7285f245618cb64499147958b8bc3ffb1c4dd504
nntruong112 <truongnnse184834@fpt.edu.vn>
2025-05-31 11:07:42 +0700

    init spring boot project

---
+++ back-end/src/main/java/SWD392/Bloodline_DNA_Testing_System/BackendApplication.java
@@ -0,0 +1,13 @@
+package SWD392.Bloodline_DNA_Testing_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/adntest/adn_test_system/AdnTestSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adntest.adn_test_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdnTestSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
