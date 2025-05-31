b3c8a1c70a756bfd27d5d057009921ffd4338f55
p1thong <phamnhatthong1712@gmail.com>
2025-05-31 13:37:47 +0700

    account crud api and jwt

---
+++ back-end/src/main/java/com/adntest/adn_test_system/configuration/OpenApiConfig.java
@@ -0,0 +1,37 @@
+package com.adntest.adn_test_system.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class OpenApiConfig {
+    
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
+                .info(new Info()
+                        .title("ADN Test System API")
+                        .description("RESTful API for ADN Test System")
+                        .version("1.0")
+                        .contact(new Contact()
+                                .name("ADN Test System")
+                                .email("contact@adntest.com")
+                                .url("https://adntest.com")));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
+    }
+} 
---
+++ back-end/src/main/java/com/adntest/adn_test_system/exception/AuthException.java
@@ -0,0 +1,7 @@
+package com.adntest.adn_test_system.exception;
+
+public class AuthException extends RuntimeException {
+    public AuthException(String message) {
+        super(message);
+    }
+} 
